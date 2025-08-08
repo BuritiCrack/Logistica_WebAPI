d086db6c6298f8f04f613037eec76b4a84142539
BuritiCrack <jmbur@BURITICRACK>
2025-08-08 18:29:13 -0500

    Add DidAttend property to EventUser class
    Introduces a new boolean property `DidAttend` to the `EventUser` class, initialized to `false`, with a display attribute for clarity. The existing `IsEventCancelled` property has been removed, and its corresponding database column has been renamed to `DidAttend` in the migration file. The `DataContextModelSnapshot.cs` has been updated to reflect these changes, ensuring the new property is included in the Entity Framework model. Other properties like `LastUpdated`, `EventId`, `AdminComments`, and `RegistrationDate` remain unchanged.

---
+++ Logistico_WebAPI/LogisticoWebAPI.Backend/Migrations/20250808232750_AddDidAttendPropertyToEventUserClass.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LogisticoWebAPI.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDidAttendPropertyToEventUserClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsEventCancelled",
+                table: "EventUsers",
+                newName: "DidAttend");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DidAttend",
+                table: "EventUsers",
+                newName: "IsEventCancelled");
+        }
+    }
+}
--- Logistico_WebAPI/LogisticoWebAPI.Shared/Entities/EventUser.cs
+++ Logistico_WebAPI/LogisticoWebAPI.Shared/Entities/EventUser.cs
@@ -13,17 +13,17 @@ namespace LogisticoWebAPI.Shared.Entities
         public string UserId { get; set; } = null!;
         public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
 
-        [Display(Name ="Estado de la aplicacion")]
+        [Display(Name = "Estado de la aplicacion")]
         public ApplicationStatus Status { get; set; } = ApplicationStatus.Pending;
-        
+
         [Display(Name = "Comentarios del administrador")]
         [MaxLength(500, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
         public string? AdminComments { get; set; }
-        
+
         [Display(Name = "Fecha de última actualización")]
         public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
-        
-        public bool IsUserConfirmed => Status == ApplicationStatus.Accepted;
-        public bool IsEventCancelled { get; set; }
+
+        [Display(Name = "¿Asistió al evento?")]
+        public bool DidAttend { get; set; } = false;
     }
 }
